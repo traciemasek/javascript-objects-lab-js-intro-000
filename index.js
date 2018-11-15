99b799457b5739c663d5ddb430c43f1c97d3079c
Tracie Masek <traciemasek@gmail.com>
2018-11-15 20:44:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+ 
+ return object; 
+}
